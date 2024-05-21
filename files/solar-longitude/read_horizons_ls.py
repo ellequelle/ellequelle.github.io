cdb3abb7e21e5f374136d162e7b9616e48db3464
Elle Hanson <elleh3113@gmail.com>
2024-05-21 16:17:29 -0400

    Ls try again

--- files/solar-longitude/read_horizons_ls.py
+++ files/solar-longitude/read_horizons_ls.py
@@ -36,6 +36,10 @@ for fn in file_paths:
 
     #  Date__(UT)__HR:MN, , , hEcl-Lon,hEcl-Lat,      PlAng,  Tru_Anom,  App_Lon_Sun,
     df = pd.read_csv(StringIO(s), names=["date",0,1,"lon","lat","plang","truanom","Ls"], index_col=False)
+    dt = pd.to_datetime(df.date)
+    lx = (dt >= "1960-01-01") & (dt <= "2070-01-01") & df["date"].str.contains("-Jan-")
+    df = df.loc[lx]
     df["date"] = df["date"].str.strip().str[:11]
     df["Ls"] = df["Ls"].round(1)
+    df[["date","Ls"]].set_index("date").to_json(f"../../_data/{planet}.json", orient="table")
     df[["date","Ls"]].set_index("date").to_json(f"{planet}.json", orient="table")
