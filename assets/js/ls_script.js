174f8917b5f3b936da26bfa9d379cdf67269eddc
Elle Hanson <elleh3113@gmail.com>
2024-05-25 16:44:39 -0400

    move high-res Ls files, add MY and MSD to mars table

--- assets/js/ls_script.js
+++ assets/js/ls_script.js
@@ -1,4 +1,8 @@
-function showPlanet(e,pname) {
+const planetList = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto',
+      'cp67p', 'eros', 'ceres', 'bennu', 'didymos', 'arrokoth'];
+
+function showPlanet(e, pname) {
+    e.target.textContent.toLowerCase()
     els = document.getElementsByClassName("tablediv");
     for (i = 0; i < els.length; i++) {
       el = els[i];
@@ -10,7 +14,7 @@ function showPlanet(e,pname) {
 
   function getLsPlanet(date_int, planet) {
     tableid = '#ls-'+planet;
-    if (planet == '67p') tableid = '#ls-_67p';
+    /* if (planet == '67p') tableid = '#ls-_67p'; */
     table_rows = document.querySelectorAll(tableid + ' > tbody:nth-child(2) > tr');
     let dt0 = 0;
     let dt1 = 0;
@@ -43,10 +47,76 @@ function showPlanet(e,pname) {
     return ls.toFixed(1);
   }
 
-  function initLsTable(e) {
-    planetList = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto',
-      '67p', 'eros', 'ceres', 'bennu', 'didymos', 'arrokoth'];
+  function getDateJSONFileExt(date_int) {
 
+    dt = date_int;
+    /* split up into multiple smaller files */
+    if (date_int <= Date.parse("1980-01-01")) {
+        return '-a';
+    } else if (dt > Date.parse("1980-01-01") & dt <= Date.parse("2000-01-01")) {
+        return '-b';
+    } else if (dt > Date.parse("2000-01-01") & dt <= Date.parse("2025-01-01")) {
+        return '-c';
+    } else if (dt > Date.parse("2025-01-01") & dt <= Date.parse("2050-01-01")) {
+        return '-d';
+    } else if (dt > Date.parse("2050-01-01") & dt <= Date.parse("2100-01-01")) {
+        return '-e';
+    } else if (dt > Date.parse("2100-01-01") & dt <= Date.parse("2200-01-01")) {
+        return '-f';
+    }
+  }
+
+
+
+  function getLsPlanetTime(date_int, planet) {
+    /* Send XMLHTTP request to get Ls from json data file. */
+    fileExt = getDateJSONFileExt(date_int);
+    fn = '/assets/data/solar-longitude/ls-hires/hires-' + planet + fileExt + '.json';
+
+    /* Send request to get JSON table (list of rows). */
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", fn, false);
+    xmlhttp.send();
+    var dtable = JSON.parse(xmlhttp.responseText)['data'];
+
+    /* Find date in table. */
+    let dt0 = 0;
+    let dt1 = 0;
+    let ls0 = 0;
+    let ls1 = 0;
+    for (i=0; i<dtable.length; i++) {
+      trow = dtable[i];
+      trdate_int = Date.parse(trow['date']); /* integer date */
+      trls = parseFloat(trow['Ls']); /* ls value */
+      /* look for smallest */
+      if (trdate_int <= date_int) {
+        dt0 = trdate_int;
+        ls0 = trls;
+      } else if (trdate_int >= date_int) {
+        dt1 = trdate_int;
+        ls1 = trls;
+        break;
+      }
+    }
+    // yearEnd = false;
+    if (ls1 < ls0) {
+      /* account for crossing end of year */
+    //   yearEnd = true;
+      ls1 = ls1 + 360.0;
+    }
+    /* linear interpolation to estimate Ls at date */
+    dt = dt1 - dt0;
+    dls = ls1 - ls0;
+    ls = ls0 + dls / dt*(date_int - dt0);
+    if (ls > 360) {
+      ls = ls - 360.0;
+    }
+    
+    /* return Ls */
+    return ls;
+  }
+
+  function initLsNowTable(e) {
     datenow = new Date();
     datenow_int = datenow.getTime();
 
@@ -58,14 +128,87 @@ function showPlanet(e,pname) {
     for (let i=0; i<planetList.length; i++) {
       let pl = planetList[i];
       let pname = pl[0].toUpperCase() + pl.substr(1);
-      if (pname == '67p') pname = '67P/Churyumov–Gerasimenko';
+      if (pname.toLowerCase() == 'cp67p') pname = '67P/Churyumov–Gerasimenko';
       let row = table.insertRow();
       let cell0 = row.insertCell();
       cell0.appendChild(document.createTextNode(pname));
       cell1 = row.insertCell();
-      cell1.appendChild(document.createTextNode(getLsPlanet(datenow_int, pl)));
+      cell1.appendChild(document.createTextNode(getLsPlanetTime(datenow_int, pl).toFixed(1)));
     }
   }
 
+  function initPage() {
 
-  window.onload = initLsTable;
+    // handle collapsible
+    var coll = document.getElementsByClassName("pcollapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("pactive");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+            content.style.display = "none";
+            } else {
+            content.style.display = "block";
+            }
+            DataTable.tables({ visible: true, api: true }).columns.adjust();
+        });
+    }
+
+    // calculate current Ls values for table
+    initLsNowTable();
+
+    for (let i=0; i<planetList.length; i++) {
+        column_list = [{ data : 'date' }, { data : 'Ls' }];
+        planet = planetList[i];
+        if (planet.toLowerCase() == 'mars') {
+            column_list = [{ data : 'date' }, { data : 'MY' }, { data : 'Ls' }, { data : 'MSD' } ];
+        }
+        let dtable = new DataTable('#ls-' + planet, {
+            ajax : 'assets/data/solar-longitude/' + planet + '.json',
+            columns : column_list,
+            // scrollY : true,
+            scrollX : false,
+            lengthMenu : [20, 30, 50, 100, 'All'],
+            columnDefs: {
+                targets : -1,
+                className : 'dt-center',
+            },
+            // displayStart: 20,
+            initComplete : function () {
+                this.api()
+                    .columns()
+                    .every(function () {
+                        let column = this;
+                        let title = column.footer().textContent;
+         
+                        // Create input element
+                        let input = document.createElement('input');
+                        input.placeholder = title;
+                        column.footer().replaceChildren(input);
+         
+                        // Event listener for user input
+                        input.addEventListener('keyup', () => {
+                            if (column.search() !== this.value) {
+                                column.search(input.value).draw();
+                            }
+                        });
+                    });
+            },
+            })
+    }
+
+    /*
+    new DataTable('#dt-mercury', {
+        ajax : 'assets/data/solar-longitude/mercury.json',
+        columns : [
+            { data : 'date' },
+            { data : 'Ls' }
+        ]
+    });
+    */
+
+  }
+
+  window.onload = initPage;
--- files/solar-longitude/read_horizons_ls.py
+++ files/solar-longitude/read_horizons_ls.py
@@ -1,12 +1,13 @@
 from io import StringIO
 import pandas as pd
+import numpy as np
 
 file_paths = (
 "horizons_results_eros.txt",
 "horizons_results_didymos.txt",
 "horizons_results_bennu.txt",
 "horizons_results_arrokoth.txt",
-"horizons_results_67p.txt",
+"horizons_results_cp67p.txt",
 "horizons_results_ceres.txt",
 "horizons_results_jupiter.txt",
 "horizons_results_mars.txt",
@@ -21,7 +22,7 @@ file_paths = (
 
 planet_list = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 
                'saturn', 'uranus', 'neptune', 'pluto',
-      '67p', 'eros', 'ceres', 'bennu', 'didymos', 'arrokoth',
+      'cp67p', 'eros', 'ceres', 'bennu', 'didymos', 'arrokoth',
       ]
 
 def ingest_horizon_ephem(fn):
@@ -43,7 +44,45 @@ def ingest_horizon_ephem(fn):
     df = pd.read_csv(StringIO(s), names=["date",0,1,"lon","lat","plang","truanom","Ls"], index_col=False)
     return df.drop(columns=[0,1])
 
+def do_extra_mars(df):
+    # calculate julian date
+    df["date_ts"] = pd.to_datetime(df["date"])
+    df["JD"] = (df["date_ts"] - pd.to_datetime("1970-01-01")).astype(int)/1e9/86400 + 2440587.5
+    # calculate Mars Standard Date (from eq 32 in Allison+2020,
+    #  neglecting any difference between TAI and UTC)
+    # df["MSD"] = (df["JD"] - 2405522)/1.02749
+    df["MSD"] = (df["JD"] - 2400000.5 - 51549.0)/1.02749125 + 44796.0
+
+    # Mars Standard Date at Ls 0, MY 0 (approximate)
+    # MSD_MY0 = 28223.6 # 1953-05-24, JD 2434521.743054128
+    JD_MY0 = 2434521.743054128 # ~1953-05-24, ~28223.6
+    # years per JD
+    YR_JD = 686.980 # d
+    dJD = (df["JD"] - JD_MY0)
+    # calculate Mars Year number (from Clancy+2000)
+    df["MY"] = np.abs(dJD)//YR_JD*np.sign(dJD)
+    df.loc[df["JD"]<JD_MY0, "MY"] += -1
+    df["MY"] = df["MY"].astype(int)
+
+    # sol dates for Mars landers
+    # pathfinder sol (local time, starting at sol 1)
+    df["pathfinder"] = df["MSD"] - 43905 - (2 + (13 + 1/60)/60)/24 + 1
+    # spirit sol (local time, starting at sol 1)
+    df["spirit"] = df["MSD"] - 46216 + (11 + (0 + 4/60)/60)/24 + 1
+    # opportunity sol (local time, starting at sol 1)
+    df["opportunity"] = df["MSD"] - 46236 - (1 + (1 + 6/60)/60)/24 + 1
+    # phoenix sol (local time, starting at sol 0)
+    df["phoenix"] = df["MSD"] - 47776 - (8 + (26 + 36/60)/60)/24
+    # curiosity sol (local time, starting at sol 0)
+    df["curiosity"] = df["MSD"] - 49269 + (9 + (9 + 41/60)/60)/24
+    # insight sol (local time, starting at sol 0)
+    df["insight"] = df["MSD"] - 51511 + (9 + (3 + 53/60)/60)/24
+    # perseverance sol (local time, starting at sol 0)
+    df["perseverance"] = df["MSD"] - 52304 + (5 + (9 + 43/60)/60)/24
+    return df[["date", "Ls", "MSD", "MY"]]
+
 for planet in planet_list:
+    print(planet)
     fn = "horizons_results_" + planet + ".txt"
 
     df = ingest_horizon_ephem("horizons_output/"+fn)
@@ -60,38 +99,50 @@ for planet in planet_list:
     df = df.loc[lx]
     df["date"] = df["date"].str.strip().str[:11]
     df["Ls"] = df["Ls"].round(1)
+    # planet-specific processing
+    if planet.lower() == "mars":
+        df = do_extra_mars(df)
+    else:
+        df = df[["date","Ls"]]
+    # fsdssaf
     fn = f"{planet}.json"
-    if planet == "67p":
-        fn = "_"+fn
-    df[["date","Ls"]].set_index("date").to_json("../../_data/"+fn, orient="table")
-    df[["date","Ls"]].set_index("date").to_json(fn, orient="table")
+    df.set_index("date").to_json("../../_data/"+fn, orient="table")
+    df.set_index("date").to_json("../../assets/data/solar-longitude/"+fn, orient="table")
+    df.set_index("date").to_json(fn, orient="table")
 
-    if planet in ("didymos", "arrokoth"):
-        continue
+    # if planet in ("didymos", "arrokoth"):
+    #     continue
     fn_hires = "horizons_hires/horizons_results_hires-" + planet + ".txt"
     df = ingest_horizon_ephem("horizons_output/"+fn_hires)
+    # planet-specific processing
+    if planet.lower() == "mars":
+        df = do_extra_mars(df)
+    else:
+        df = df[["date","Ls"]]
+    
     df["date"] = df["date"].str.strip().str[:11]
     df["Ls"] = df["Ls"].round(3)
-    df = df[["date","Ls"]].set_index("date")
-    fn = f"../../_data/ls-hires/hires-{planet}.json"
+    df = df.set_index("date")
+    fn = f"../../assets/data/solar-longitude/ls-hires/hires-{planet}.json"
+    # fn = f"../../_data/ls-hires/hires-{planet}.json"
 
     dt = pd.to_datetime(df.index)
 
-    # split up into multiple smaller files
-    lx = (dt <= "1980-01-01")
-    df.loc[lx].to_json(fn.replace(".json", "-a.json"))
+    # split up into multiple smaller files with 3 months overlap
+    lx = (dt <= "1980-03-05")
+    df.loc[lx].to_json(fn.replace(".json", "-a.json"), orient="table")
 
-    lx = (dt > "1980-01-01") & (dt <= "2000-01-01")
-    df.loc[lx].to_json(fn.replace(".json", "-b.json"))
+    lx = (dt >= "1979-09-01") & (dt <= "2000-03-05")
+    df.loc[lx].to_json(fn.replace(".json", "-b.json"), orient="table")
 
-    lx = (dt > "2000-01-01") & (dt <= "2025-01-01")
-    df.loc[lx].to_json(fn.replace(".json", "-c.json"))
+    lx = (dt >= "1999-09-01") & (dt <= "2025-03-05")
+    df.loc[lx].to_json(fn.replace(".json", "-c.json"), orient="table")
 
-    lx = (dt > "2025-01-01") & (dt <= "2050-01-01")
-    df.loc[lx].to_json(fn.replace(".json", "-d.json"))
+    lx = (dt >= "2024-09-01") & (dt <= "2050-03-05")
+    df.loc[lx].to_json(fn.replace(".json", "-d.json"), orient="table")
 
-    lx = (dt > "2050-01-01") & (dt <= "2100-01-01")
-    df.loc[lx].to_json(fn.replace(".json", "-e.json"))
+    lx = (dt >= "2049-09-01") & (dt <= "2100-03-05")
+    df.loc[lx].to_json(fn.replace(".json", "-e.json"), orient="table")
 
-    lx = (dt > "2100-01-01") & (dt <= "2200-01-01")
-    df.loc[lx].to_json(fn.replace(".json", "-f.json"))
+    lx = (dt >= "2099-09-01") & (dt <= "2200-03-05")
+    df.loc[lx].to_json(fn.replace(".json", "-f.json"), orient="table")
